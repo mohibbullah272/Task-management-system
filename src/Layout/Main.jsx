ed86d7d42ca097cc4ce059d20c565d88429a219d
mohibbullah272 <bullahmohi50@gmail.com>
2025-02-20 12:05:19 +0600

    create initial setup of daily task management system

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ThemeSwitcher from '../shared/ThemeSwitcher';
+
+const Main = () => {
+    return (
+        <div>
+            <p className='text-3xl text-red-950'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, non?</p>
+            <ThemeSwitcher></ThemeSwitcher>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './Layout/Main.jsx';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+  },
+]);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+     <RouterProvider router={router} />
+  </StrictMode>,
+)
